9bc3557862d9c6ac1233bcaa4a46f8a366ebf480
Mustafa <mustafa.visionet@gmail.com>
2021-12-16 15:06:07 +0700

    fix: return accidentally removed cc email

--- application/views/headers/head.php
+++ application/views/headers/head.php
@@ -17,6 +17,8 @@
 <link href="<?php echo base_url('assets/css/navigation.css') ?>" rel="stylesheet" media="screen,print">
 <link href="<?php echo base_url('assets/css/style.css') ?>" rel="stylesheet" media="screen,print">
 
+
+<script src="<?=base_url('assets/bootstrap-3.2.0/js/bootstrap.min.js') ?>"></script>
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
